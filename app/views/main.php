53bd8a39e83d83db3d341a2c356e99ace3746e11
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2014-09-13 01:31:31 -0500

    Bathroom Break

--- app/views/main.php
+++ app/views/main.php
@@ -22,6 +22,7 @@
 <link media="screen, print" type="text/css" rel="stylesheet" href="./MarketingWeb/internet_tip/shared-content-library/config/css/jquery.megamenu.css"/>
 <link media="screen, print" type="text/css" rel="stylesheet" href="./MarketingWeb/internet_tip/shared-content-library/config/css/jquery.slideshow.css"/>
 <link media="screen, print" type="text/css" rel="stylesheet" href="./MarketingWeb/internet_tip/shared-content-library/config/css/content-homepage-buttons.css"/>
+<link media="screen, print" type="text/css" rel="stylesheet" href="./MarketingWeb/internet_tip/shared-content-library/config/css/bits-plz.css"/>
 <!--[if IE 7]>
     <link href="./MarketingWeb/internet_tip/shared-content-library/config/css/content-ie7.css" rel="stylesheet" type="text/css" media="screen, print" />
   <![endif]-->
@@ -371,16 +372,100 @@
 <div id="">
     
   <div id="wrapper2">
+
     <div class="container">
+
       <div id="section3">
+
         <div id="main-nav">
 
-        </div>
-        <div id="content-pane">
+          <ul class="side-nav-list">
+            <li><a href="">Accounts</a></li>
+            <li class="selected-nav-item"><a href="">Child Accounts</a></li>
+            <li><a href="">AutoPayments</a></li>
+            <li><a href="">Account Reports</a></li>
+          </ul> 
 
         </div>
+
+        <div id="content-pane">
+          
+          <h1 id="accounts-title">
+            Accounts
+          </h1>
+
+          <div class="account-view">
+            <div class="account-header">
+              <span class="account-name">Checking Account</span>
+              <span class="account-no-label">Acct. No:</span>
+              <span class="account-no">14F54KHS409</span>
+              <span class="account-bal-label">Balance:</span>
+              <span class="account-balance">$19,355.01</span>
+            </div>
+            
+            <div class="account-detail-view">
+              <div class="activity-container">
+                <span class="activity-label">View Activity in: </span>
+                <select name="">
+                  <option value="">This Week</option>
+                  <option value="">This Month</option>
+                  <option value="">This Year</option>
+                  <option value="">All Time</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="account-view">
+            <div class="account-header">
+              <span class="account-name">Savings Account</span>
+              <span class="account-no-label">Acct. No:</span>
+              <span class="account-no">BE36BCW983</span>
+              <span class="account-bal-label">Balance:</span>
+              <span class="account-balance">$7,238.33</span>
+            </div>
+            
+            <div class="account-detail-view">
+              <div class="activity-container">
+                <span class="activity-label">View Activity in: </span>
+                <select name="">
+                  <option value="">This Week</option>
+                  <option value="">This Month</option>
+                  <option value="">This Year</option>
+                  <option value="">All Time</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="account-view">
+            <div class="account-header">
+              <span class="account-name">Checking Account 2</span>
+              <span class="account-no-label">Acct. No:</span>
+              <span class="account-no">G4C0M2D782J</span>
+              <span class="account-bal-label">Balance:</span>
+              <span class="account-balance">$59,004.70</span>
+            </div>
+            
+            <div class="account-detail-view">
+              <div class="activity-container">
+                <span class="activity-label">View Activity in: </span>
+                <select name="">
+                  <option value="">This Week</option>
+                  <option value="">This Month</option>
+                  <option value="">This Year</option>
+                  <option value="">All Time</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+        </div>
+     
       </div> <!-- section3 --> 
+    
     </div> <!-- container --> 
+  
   </div> <!-- wrapper2 --> 
 
 </div> <!-- wrapper -->
