29143777e033822f2aaca50fc413e61534416cf6
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2014-09-13 11:24:04 -0500

    I don't even know what I'm doing anymore

--- app/views/transactions.blade.php
+++ app/views/transactions.blade.php
@@ -1,4 +1,4 @@
-@include('partials.header');
+@include('partials.header')
 
 <div id="">
 
@@ -11,15 +11,58 @@
         <div id="main-nav">
 
           <ul class="side-nav-list">
-            <li><a href="">Accounts</a></li>
-            <li class="selected-nav-item"><a href="">Budgets</a></li>
-            <li><a href="">AutoPayments</a></li>
-            <li><a href="">Account Reports</a></li>
+            <li><a href="">Transactions</a></li>
+            <li class="selected-nav-item"><a href="">Tag By Person</a></li>
+            <li><a href="">Tag by Category</a></li>
+            <li><a href="">Filter Transactions</a></li>
           </ul>
 
         </div>
 
         <div id="content-pane">
+        	<h1 id="accounts-title">
+            Transaction History
+          </h1>
+
+          <!-- $('td').keypress(function(evt){
+				    if(evt.which == 13 || evt.keyCode == 13){
+				        event.preventDefault();
+				        alert('Hi');
+				        var cellindex = $(this).index()
+				        var rowindex = $(this).parents('tr').index() + 1
+				        $(this).parents('table').find('tr:eq('+rowindex+') td:eq('+cellindex+')').focus()
+				    }
+				  });-->
+          <table id="transaction-list">
+          	<thead>
+          		<th><a href="">Date</a></th>
+          		<th><a href="">Post Date</a></th>
+          		<th><a href="">Description</a></th>
+          		<th class="selected"><a href="">Category</a></th>
+          		<th><a href="">Person</a></th>
+          		<th><a href="">Amount</a></th>
+          	</thead>
+          	<tr>
+          		<td>11-20-2014</td>
+          		<td>11-21-2014</td>
+          		<td contenteditable>Bought Crack Cocaine</td>
+          		<td contenteditable></td>
+          		<td contenteditable></td>
+          		<td class="amount">3,467.21</td>
+          	</tr>
+          	<tr>
+          		<td>11-20-2014</td>
+          		<td>11-23-2014</td>
+          		<td contenteditable>Bought Your Mom</td>
+          		<td contenteditable></td>
+          		<td contenteditable></td>
+          		<td class="amount">3.70</td>
+          	</tr>
+          </table>
+
+
+
+
 
         </div>  
 
@@ -31,4 +74,4 @@
 
 </div> <!-- wrapper -->
 
-@include('partials.footer');
+@include('partials.footer')
