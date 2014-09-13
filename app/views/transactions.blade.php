424c29c7b2e4345fa8997740f1de8f012e1549f0
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2014-09-13 10:19:26 -0500

    Transaction page frame

---
+++ app/views/transactions.blade.php
@@ -0,0 +1,34 @@
+@include('partials.header');
+
+<div id="">
+
+  <div id="wrapper2">
+
+    <div class="container">
+
+      <div id="section3">
+
+        <div id="main-nav">
+
+          <ul class="side-nav-list">
+            <li><a href="">Accounts</a></li>
+            <li class="selected-nav-item"><a href="">Budgets</a></li>
+            <li><a href="">AutoPayments</a></li>
+            <li><a href="">Account Reports</a></li>
+          </ul>
+
+        </div>
+
+        <div id="content-pane">
+
+        </div>  
+
+      </div> <!-- section3 -->
+
+    </div> <!-- container -->
+
+  </div> <!-- wrapper2 -->
+
+</div> <!-- wrapper -->
+
+@include('partials.footer');
