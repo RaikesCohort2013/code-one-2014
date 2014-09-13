edb89d084178e7e8f2acfc6cb59468b51171d78b
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2014-09-13 05:49:07 -0500

    Pizza spider trampoline'

--- public/res/js/views/post-view.js
+++ public/res/js/views/post-view.js
@@ -19,6 +19,13 @@ define([
                 {value: 15, label: 'bar'},
                 {value: 10, label: 'baz'},
                 {value: 5, label: 'A really really long label'}
+              ],backgroundColor: '#ccc',
+              labelColor: '#000',
+              colors: [
+                '#0D723c',
+                '#3D925c',
+                '#6DA27c',
+                '#9DC29c'
               ],
               formatter: function (x) { return x + "%"},
               resize: true
@@ -28,14 +35,23 @@ define([
           Morris.Bar({
             element: 'barGraph',
             data: [
-              {x: '1/14', y: 30},
-              {x: '2/14', y: 26},
-              {x: '3/14', y: 31},
-              {x: '4/14', y: 18}
+              {x: '1/14', y: '30'},
+              {x: '2/14', y: '26'},
+              {x: '3/14', y: '31'},
+              {x: '4/14', y: '18'}
             ],
             xkey: 'x',
             ykeys: ['y'],
-            labels: ['Amount Saved']
+            labels: [''],
+            barColors: function (row, series, type) {
+              if (type === 'bar') {
+                var green = Math.ceil(255 * row.y / this.ymax);
+                return 'rgb(12,' + green + ',70)';
+              }
+              else {
+                return '#000';
+              }
+            }
           });
         }
     })
