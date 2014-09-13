16dfce09195bfb488c51ae1e34723a087aaecad9
Brobin <tobinjbrown@gmail.com>
2014-09-13 08:57:25 -0500

    gosh darn merge conflicts...

--- app/models/Transaction.php
+++ app/models/Transaction.php
@@ -8,20 +8,9 @@ class Transaction extends BaseModel
 		return Account::where('account_number', $this->account_number)->first();
 	}
 
-<<<<<<< HEAD
 	public function mccCode()
 	{
 		return MccCode::where('sic_code', $this->mcc)->first();
-=======
-	public function mcc_code()
-	{
-		return MccCode::where('sic_code', $this->mcc)->first();
-	}
-
-	public function date()
-	{
-		return strtotime($this->post_date);
->>>>>>> bb798634ad4faafd303b9567445834813f7d2dcf
 	}
 	
 }
